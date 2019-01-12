a0a83c84e733e03c06fe5291c0be996500859efc
zaiverse <brenda.garcia007@mymdc.net>
2019-01-11 22:32:02 -0500

    fixed the changing questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,20 +3,18 @@ var correctAnswers = 0;
 var wrongAnswers = 0;
 var currentQuestion = 0;
 var counter = 20;
-var questionNumber = 0;
-let trivia;
 
 //variable that stores questions, choices, and answers for the trivia
 var Questions = [
         {
             question: "what is the largest sea animal",
             choices: ["blue whale", "crab", "me"],
-            answer: 1
+            correctAnswer: "blue whale"
         },
         {
             question: "How deep is the ocean",
             choices: ["unknown", "10,994 meters", "sometin"],
-            answer: 2
+            correctAnswer: "10,994 meters"
         }
     ]
 
@@ -43,16 +41,51 @@ var Questions = [
             $('#timer').text('Time remaining: ' + --counter);
             }
             if(counter <= 0){
-                timeOut();
+                alert("timesup")
             }
         }, 1000)
     }
 
+    function reset(){
+        $("#questionDiv").empty();
+        $('#answerChoices').empty();
+    }
+    
+    //function for question display
     function displayQuestion(){
-        $('#questionDiv').text(Questions[this.currentQuestion].question);
+        timer();
+        reset();
+        $('#questionDiv').append("<h5>" + Questions[this.currentQuestion].question + "</h5>");
         for(var i=0; i< Questions[this.currentQuestion].choices.length; i++){
-            $('#answerChoices').append("<div><button class='answer-button' id='button' data-name='" + Questions[this.currentQuestion].choices[i]
+            $('#answerChoices').append("<div><button class='answer-button' id='button' class='choiceButton' data-name='" + Questions[this.currentQuestion].choices[i]
             + "'>" + Questions[this.currentQuestion].choices[i] + "</button></div>");
         }
     }
-  
+    
+    //create function to iterate currentQuestion by one 
+    function nextQuestion(){
+            currentQuestion++;
+            displayQuestion();
+    }
+    //check whether button clicked is === to correctAnswer
+    function checkAnswer(event){
+        if($(event.target).attr("data-name") === Questions[this.currentQuestion].correctAnswer){
+            correctAnswers++;
+            console.log(correctAnswers);
+            nextQuestion();
+        }else{
+            wrongAnswers++;
+            console.log(wrongAnswers);
+        }
+    }
+    function timesUp(){
+
+    }
+
+    function userAnswer(){
+
+    }
+//check which button was clicked by the user and execute checkAnswer function
+$(document).on("click", "#button", function(event){
+    checkAnswer(event);
+})
